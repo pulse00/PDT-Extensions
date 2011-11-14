da16da4525e870634821b003b439553db96241e5
Robert Gruendler <r.gruendler@gmail.com>
2011-11-14 22:32:16 +0100

    added license headers

--- com.dubture.pdt.ui/src/com/dubture/pdt/ui/template/PDTTemplateContext.java
+++ com.dubture.pdt.ui/src/com/dubture/pdt/ui/template/PDTTemplateContext.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * This file is part of the PDT Extensions eclipse plugin.
+ * 
+ * (c) Robert Gruendler <r.gruendler@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ ******************************************************************************/
 package com.dubture.pdt.ui.template;
 
 import org.eclipse.dltk.core.ISourceModule;
