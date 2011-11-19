0e8b337ca3faaa3c55cad07fadbc84f5f5a45817
Robert Gruendler <robert@dubture.com>
2011-11-19 20:00:45 +0100

    added tests

---
+++ com.dubture.pdt.test/src/com/dubture/pdt/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.dubture.pdt.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CodeGenerationTests.class })
+public class AllTests {
+
+}
---
+++ com.dubture.pdt.test/src/com/dubture/pdt/test/CodeGenerationTests.java
@@ -0,0 +1,29 @@
+package com.dubture.pdt.test;
+
+import junit.framework.TestCase;
+
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.php.internal.core.typeinference.FakeMethod;
+import org.junit.Test;
+
+import com.dubture.pdt.ui.codemanipulation.CodeGeneration;
+
+@SuppressWarnings("restriction")
+public class CodeGenerationTests extends TestCase {
+	
+	@Test
+	public void testMethodGeneration() {
+		
+		try {
+			
+			FakeMethod fMethod = new FakeMethod(null, "test");		
+			String stub = CodeGeneration.getMethodStub(null, fMethod, "\t", "\n", false);			
+			assertEquals(68, stub.length());			
+			assertEquals("test()", stub.substring(17, 23));
+
+			
+		} catch (ModelException e) {
+			fail();
+		}		
+	}
+}
